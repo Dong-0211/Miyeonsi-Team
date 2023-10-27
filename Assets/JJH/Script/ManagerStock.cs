2fc62c7122d9108c4e332f820da8cc92ade1b681
backsani <118903562+backsani@users.noreply.github.com>
2023-10-27 20:41:10 +0900

    10/27 주식 시스템 및 리스트
    구현

--- Assets/JJH/Script/ManagerStock.cs
+++ Assets/JJH/Script/ManagerStock.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [System.Serializable]
 public class ManagerStock 
@@ -6,11 +7,17 @@ public class ManagerStock
 
     [SerializeField] public string stockName;
     [SerializeField] public int stockNumber;
-    [SerializeField] public float startPrice;
+    [SerializeField] public int startPrice;
     [SerializeField] public float endPrice;
     [SerializeField] public float increacePricePercent;
     [SerializeField] public int increaceWidth;
     [SerializeField] public int decreaceWidth;
 
+}
+
+[CreateAssetMenu(fileName = "Stock", menuName = "Stock/StockValue")]
+public class Stock : ScriptableObject
+{
+    public List<ManagerStock> Stocks = new List<ManagerStock>();
 
 }
--- Assets/JJH/Script/Stock.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "Stock", menuName = "Stock/StockValue")]
-public class Stock : ScriptableObject
-{
-    public List<ManagerStock> StockInt = new List<ManagerStock>();
-   
-}
