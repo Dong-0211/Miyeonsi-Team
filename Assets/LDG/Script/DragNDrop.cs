a8531ba695fbf378c2f6f068fff4d61e407ebeb0
Dong-0211 <k010211@naver.com>
2024-01-09 21:47:38 +0900

    01.09_작업
    드래그앤드랍 후 충돌처리중 => 해결못함

--- Assets/LDG/Script/DragNDrop.cs
+++ Assets/LDG/Script/DragNDrop.cs
@@ -6,31 +6,42 @@ using UnityEngine.EventSystems;
 
 public class DragNDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    [SerializeField] Vector2 Default_Pos;
+    //[SerializeField] Vector2 Default_Pos;
     [SerializeField] Vector2 Current_Pos;
-    //[SerializeField] Vector2 Mouse_Pos;
     [SerializeField] GameObject Copy_To_Do;
+    [SerializeField] GameObject Work1;
+
+    DayBox_Function DayBox_Function;
+
+    void Start()
+    {
+        Work1 = Resources.Load<GameObject>("Prefab/Work1");
+    }
+
+    void Update()
+    {
+    }
 
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        //Default_Pos = this.transform.position;
-        Copy_To_Do = Instantiate(this.gameObject);
-        Copy_To_Do.transform.parent = GameObject.Find("Canvas").transform;
+        Copy_To_Do = Instantiate(Work1);
+        //Copy_To_Do.GetComponent<DragNDrop>().enabled = false;
+        Copy_To_Do.transform.SetParent(GameObject.Find("Canvas").transform);
         Copy_To_Do.GetComponent<Image>().color = new Color(1.0f, 0.0627f, 0.0627f, 1.0f);
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
         Current_Pos = eventData.position;
-        //this.transform.position = Current_Pos;
         Copy_To_Do.transform.position = Current_Pos;
 
     }
 
     void IEndDragHandler.OnEndDrag(PointerEventData eventData)
     {
-        //Mouse_Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //this.transform.position = Current_Pos;
         Copy_To_Do.transform.position = Current_Pos;
+        //DayBox_Function.To_do_Pos = Current_Pos;
     }
+
+    
 }
