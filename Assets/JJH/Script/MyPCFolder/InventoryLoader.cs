f7fe22f654311c6bc0a12b86ac5a9aade04566d8
backsani <118903562+backsani@users.noreply.github.com>
2024-02-20 21:54:26 +0900

    2/20 인벤토리 코드 및 상점 코드 수정

--- Assets/JJH/Script/MyPCFolder/InventoryLoader.cs
+++ Assets/JJH/Script/MyPCFolder/InventoryLoader.cs
@@ -24,4 +24,57 @@ public class InventoryLoader : MonoBehaviour
             Slot[i].transform.Find("ItemCount").GetComponent<Text>().text = GameManager.Instance.data.inventory[i].itemCount.ToString();
         }
     }
+    /*
+     * for (int i = 0; i < 8; i++)
+        {
+            if (item.items[i] != null)
+            {
+                Slot[i].transform.Find("ItemImage").GetComponent<Image>().sprite = item.items[i].ItemImage;
+                Slot[i].transform.Find("ItemPrice").GetComponent<Text>().text = item.items[i].Price.ToString();
+                Slot[i].transform.Find("ItemName").GetComponent<Text>().text = item.items[i].ItemName;
+            }
+            else
+            {
+                Slot[i].transform.Find("ItemImage").GetComponent<Image>().sprite = null;
+                Slot[i].transform.Find("ItemPrice").GetComponent<Text>().text = "0";
+                Slot[i].transform.Find("ItemName").GetComponent<Text>().text = "아이템이 없습니다";
+
+            }
+        }
+    public void BuyItem()
+    {
+        string itemName = this.gameObject.name;
+        Transform itemPrice = transform.Find("ItemPrice");
+        float price = float.Parse(itemPrice.GetComponent<Text>().text);
+        int itemNumber = int.Parse(itemName.Substring(5, 1));
+
+
+        if (price < GameManager.Instance.data.abilities.Money)
+        {
+            GameManager.Instance.data.abilities.Money -= price;
+
+            for(int i = 0; i < 8; i++)
+            {
+                if (GameManager.Instance.data.inventory[i].itemCode == 63)
+                {
+                    
+                    GameManager.Instance.data.inventory[i].itemCode = itemNumber;
+                    GameManager.Instance.data.inventory[i].itemCount++;
+                    return;
+                }
+                else if (GameManager.Instance.data.inventory[i].itemCode != 63)
+                {
+                    if (GameManager.Instance.data.inventory[i].itemCode == itemNumber)
+                    {
+                        GameManager.Instance.data.inventory[i].itemCount++;
+                        return;
+                    }
+                }
+
+            }
+            
+        }
+
+    }
+    */
 }
--- Assets/JJH/Script/StoreFolder/StoreManager.cs
+++ Assets/JJH/Script/StoreFolder/StoreManager.cs
@@ -31,6 +31,24 @@ public class StoreManager : MonoBehaviour
 
             }
         }
+        /*
+        for (int i = 0; i < 8; i++)
+        {
+            if (item.items[i] != null)
+            {
+                Slot[i].transform.Find("ItemImage").GetComponent<Image>().sprite = item.items[i].ItemImage;
+                Slot[i].transform.Find("ItemPrice").GetComponent<Text>().text = item.items[i].Price.ToString();
+                Slot[i].transform.Find("ItemName").GetComponent<Text>().text = item.items[i].ItemName;
+            }
+            else
+            {
+                Slot[i].transform.Find("ItemImage").GetComponent<Image>().sprite = null;
+                Slot[i].transform.Find("ItemPrice").GetComponent<Text>().text = "0";
+                Slot[i].transform.Find("ItemName").GetComponent<Text>().text = "아이템이 없습니다";
+
+            }
+        }
+        */
     }
 
 
